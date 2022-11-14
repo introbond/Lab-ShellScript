5a0459d6b3cddc633da1321ea90b786e4c60575c
introbond <introbond.dev@gmail.com>
2022-11-14 09:34:00 +0700

    tidy up

--- pyenv.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-echo 'eval "$(pyenv init -)"' >> ~/.bashrc
