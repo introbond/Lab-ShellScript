b525248f815d735c8202d575c7cd976cdf3b90d2
introbond <introbond.dev@gmail.com>
2023-01-10 11:21:08 +0700

    add: check if exists condition

--- build_python_venv.sh
+++ build_python_venv.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-
 initial () {
     python -m venv env
     source env/bin/activate
@@ -9,5 +8,24 @@ install () {
     pip install -r requirements.txt
 }
 
-initial
-install
+gitignore () {
+    touch .gitignore
+    echo "env/" >> .gitignore
+}
+
+if [ -e env/ ]
+    then
+        echo "===== venv already exist ====="
+    else
+        echo "===== start create venv ====="
+        initial
+        install
+fi
+
+if [ -e .gitignore ]
+    then
+        echo "===== .gitignore already exist ====="
+    else
+        echo "===== start create .gitignore ====="
+        gitignore
+fi
